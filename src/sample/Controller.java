9016c096e2e442a19a28b6b0ab894f2d9497ab39
Cpt Wasp <StigNelson11@gmail.com>
2020-06-11 16:19:26 +0300

    sign up controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,19 @@
 package sample;
+        import java.io.IOException;
         import java.net.URL;
         import java.util.ResourceBundle;
         import javafx.collections.FXCollections;
         import javafx.collections.ObservableList;
         import javafx.fxml.FXML;
+        import javafx.fxml.FXMLLoader;
         import javafx.fxml.Initializable;
+        import javafx.scene.Parent;
+        import javafx.scene.Scene;
         import javafx.scene.control.Button;
         import javafx.scene.control.ChoiceBox;
         import javafx.scene.control.PasswordField;
         import javafx.scene.control.TextField;
+        import javafx.stage.Stage;
 
 public class Controller implements Initializable {
     ObservableList<String> cb1Array = FXCollections.observableArrayList("Клиент", "Строитель", "Дизайнер");
@@ -39,10 +44,23 @@ public class Controller implements Initializable {
         typeCheckBox.setItems(cb1Array);
         typeCheckBox.setValue("default");
 
-
-
         signOutButton.setOnAction(event->{
             System.out.println("регистрация в системе");
+            signOutButton.getScene().getWindow().hide();
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("SignUpVision.fxml"));
+
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
         });
 
 
